9f6970c28997a1877a9dd84b2c089183d986df11
trungpq <trungpq.ml@gmail.com>
2020-04-21 22:17:21 +0800

    Restructure code

--- src/config.py
+++ src/config.py
@@ -10,4 +10,13 @@ class Config:
     full_annotation_name = intermediate_data_path + "full_annotation.csv"
     batch_size = 16
 
+    processed_path = "../data/processed/"
+
+    base_learning_rate = 1e-5
+    epochs = 1 #should be 500 change to 400000 if you are a rich kid!
+    weight_decay = 0.0005
+    gamma = 0.001
+    power = 0.75
+    num_of_tries = 5
+
 cfg = Config()
